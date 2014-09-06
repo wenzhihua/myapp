068bc3568055974def943b30c279bc3ed00a645e
zhihuawen <zhihuawen@zhihuawen-macbookpro.roam.corp.google.com>
2014-09-05 21:52:01 -0700

    test

--- app/src/main/java/com/example/zhihuawen/myapplication/MyActivity.java
+++ app/src/main/java/com/example/zhihuawen/myapplication/MyActivity.java
@@ -12,6 +12,7 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        System.out.println("less");
     }
 
 
